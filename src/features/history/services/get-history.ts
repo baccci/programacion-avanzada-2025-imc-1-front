ab524725dbbf29eb81ab8ea4cc7e676ce99b92ea
carosuppo <carosu808@gmail.com>
2025-09-18 15:16:15 -0300

    Cambios en el filtro por rango de fecha

--- src/features/history/services/get-history.ts
+++ src/features/history/services/get-history.ts
@@ -4,10 +4,13 @@ export async function getHistoryService(desde?: string, hasta?: string): Promise
   try {
     const queryParams = []
     if (desde) queryParams.push(`desde=${desde}`)
+
     if (hasta) queryParams.push(`hasta=${hasta}`)
+    
     const queryString = queryParams.length ? `?${queryParams.join('&')}` : ''
 
-    const response = await fetch('https://pa2025imcbackend-production.up.railway.app/imc/historial${queryString}', {
+    // const response = await fetch(`https://pa2025imcbackend-production.up.railway.app/imc/historial${queryString}`, {
+    const response = await fetch(`http://localhost:3000/imc/historial${queryString}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
