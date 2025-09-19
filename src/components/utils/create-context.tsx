33be010adc53f8d7c5b3eaa649a236f080dafb8e
Salvador Bacci <salvador_bacci@hotmail.com>
2025-09-19 07:26:15 -0300

    fix: added missing function

--- src/components/utils/create-context.tsx
+++ src/components/utils/create-context.tsx
@@ -20,3 +20,32 @@ export function createContext<ContextValue>(useValue: () => ContextValue) {
 
   return [Provider, useContext] as const
 }
+
+export function deprecated_createContext<ContextValue>(
+  defaultValue?: ContextValue
+) {
+  const Context = React.createContext<ContextValue | undefined>(defaultValue)
+
+  const Provider = (props: {
+    value: ContextValue
+    children: React.ReactNode
+  }) => {
+    const { children, ...context } = props
+
+    const dependencyArray = Object.values(context)
+    const { value } = React.useMemo(() => context, dependencyArray)
+
+    return <Context.Provider value={value}>{children}</Context.Provider>
+  }
+
+  const useContext = () => {
+    const context = React.use(Context)
+    if (!context && !defaultValue) {
+      throw new Error('useContext must be inside a Provider with a value')
+    }
+    if (context) return context
+    return defaultValue as ContextValue
+  }
+
+  return [Provider, useContext] as const
+}
